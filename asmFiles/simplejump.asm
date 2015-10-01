fb46b5077f45d1d9ea2d4398e5e408ca83891ee1
Jinyi Zhang <zhan1182@purdue.edu>
2015-09-30 21:19:20 -0400

    add simple jump

---
+++ asmFiles/simplejump.asm
@@ -0,0 +1,19 @@
+
+main:
+	ori $2, $0, 0x1234
+	ori $3, $0, 0x1234
+	j jumptarget
+	ori $4, $0, 0x1234
+	ori $5, $0, 0x1234
+	ori $6, $0, 0x1234
+
+jt2:
+	halt
+
+
+jumptarget:
+	ori $7, $0, 0x1234
+	ori $8, $0, 0x1234
+	ori $9, $0, 0x1234
+	ori $10, $0, 0x1234
+	j jt2
