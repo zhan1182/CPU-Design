bce83f1d76803ba3f316ba6e30ae4257b41bf2a3
Jinyi Zhang <zhan1182@purdue.edu>
2015-10-21 21:07:49 -0400

    icache v1, fail branch

--- source/icache.sv
+++ source/icache.sv
@@ -6,11 +6,13 @@
  */
 
 `include "cpu_types_pkg.vh"
+`include "datapath_cache_if.vh"
+`include "cache_control_if.vh"
 
 module icache (
   input logic CLK, nRST,
-  datapath_cache_if dcif,
-  cache_control_if ccif
+  datapath_cache_if.icache dcif,
+  cache_control_if.icache ccif
 );
    import cpu_types_pkg::*;
    
@@ -20,8 +22,15 @@ module icache (
    logic [ITAG_W-1:0] tag;
    logic [IIDX_W-1:0] idx;
    logic [IBYT_W-1:0] bytoff;
+   // {valid, tag 26bits, word 32 bits}
    logic [15:0][58:0]     curr_cache, next_cache;
    int 			  i;
+   logic 		  hit;
+   logic 		  miss;
+
+   // This is a cache hit
+   assign hit = (tag == curr_cache[idx][57:57-ITAG_W+1]);
+   assign miss = ~hit;
    
 
    assign tag = dcif.imemaddr[31:31-ITAG_W+1];
@@ -32,26 +41,25 @@ module icache (
    always_ff @ (posedge CLK, negedge nRST) begin
       if (nRST == 0) begin
 	 for(i = 0; i < 16; i++)begin
-	    curr_cache[i] <= 0;
+	    curr_cache[i] <= -1;
 	 end	 
       end
-      
       else begin
-	 curr_cache <= next_cache;
+	 if(~ccif.iwait && miss)
+	   begin
+	      curr_cache[idx] <= next_cache[idx];
+	   end
       end
    end
 
-   assign ccif.iREN = (tag == cache[idx][57:57-ITAG_W+1]) ? 0:dcif.imemREN;
-   assign ccif.iaddr = (tag != cache[idx][57:57-ITAG_W+1] && dcif.imemREN) ? dcif.imemaddr : 0;
-   
-   assign next_cache[idx] = (tag == cache[idx][57:57-ITAG_W+1]) ? curr_cache[idx]:(~ccif.iwait & {1'b1, tag, ccif.iload}); // ????
-   
-   assign dcif.imemload = (dcif.imemREN) ? curr_cache[idx] : 0;
-   assign dcif.ihit = ~ccif.iwait;
- //???
-   
-   
-      
+   assign ccif.iREN = (hit) ? 0 : 1;
+   assign ccif.iaddr = dcif.imemaddr;
 
+   assign next_cache = {1'b1, tag, ccif.iload};
+
+   assign dcif.imemload = (hit) ? curr_cache[idx][31:0] : ((~ccif.iwait) ? ccif.iload : 0);
+   
+   assign dcif.ihit = hit | (~ccif.iwait && miss);
+   
 
 endmodule // caches
